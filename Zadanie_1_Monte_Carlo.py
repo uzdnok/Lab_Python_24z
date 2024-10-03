011fff7b0e4cc100b460fe09f5b1e24a00f1287b
uzdnok <konrad.kedzerski127@gmail.com>
2024-10-03 13:42:30 +0200

    zmiana nazwy pliku

---
+++ Zadanie_1_Monte_Carlo.py
@@ -0,0 +1,25 @@
+import random
+import math
+
+
+def montecarlo(n, k):
+    trafy = 0
+    for i in range(n):
+        for j in range(k):
+            x = random.random()
+            y = random.random()
+            if x * x + y * y <= 1:
+                trafy += 1
+        if i == n - 1:
+            print("Uzyskane przybliżenie: ")
+        print(4 * (trafy / (k * (i + 1))))
+
+
+n = input()
+k = input()
+n = int(n)
+k = int(k)
+montecarlo(n, k)
+
+print("Dokładna wartość Pi: ")
+print(math.pi)
